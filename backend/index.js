be467699667482c8cd806c0983450dbe9d39053f
Hansika <nostochk@gmail.com>
2024-05-29 23:20:11 +0530

    refactor with cor policy

--- backend/index.js
+++ backend/index.js
@@ -3,12 +3,29 @@ import { PORT, MONGO_URL } from "./config.js";
 import mongoose from "mongoose";
 import { Book } from "./models/bookModel.js";
 import bookRoute from "./routes/bookRoute.js";
+import cors from "cors";
 
 const app = express();
 
 //configuring middleware
 
 app.use(express.json());
+
+//middleware for handling CORS policy
+// option 1 : Allow All origins with Default cors(*)
+// not the best way
+
+//app.use(cors());
+
+//option 2: Allow custom origins
+// the better way
+app.use(
+  cors({
+    origin: "http://localhost:5555/",
+    methods: ["GET", "PUT", "POST", "DELETE"],
+    allowedHeaders: ["Content-Type"],
+  })
+);
 
 app.get("/", (req, res) => {
   console.log(req);
