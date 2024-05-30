97f3144a6ccbd1d17814a8f34aa8bf48cacca3d4
Hansika <nostochk@gmail.com>
2024-05-30 15:54:22 +0530

    implement show book list as a card

--- backend/config.js
+++ backend/config.js
@@ -2,3 +2,5 @@ export const PORT = 5555;
 
 export const MONGO_URL =
   "mongodb+srv://nostochk:lfbwxjoSZGFszEbw@book-store.hnbdybn.mongodb.net/book-store?retryWrites=true&w=majority&appName=Book-Store";
+
+
