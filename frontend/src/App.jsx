b7ef7235845abe67c1b57a95554b0adc118f4828
Hansika <nostochk@gmail.com>
2024-05-30 14:41:49 +0530

    implement delete book

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,20 @@
-import { Route, Routes } from "react-router-dom"
-import CreateBook from "./pages/CreateBook"
-import DeleteBook from "./pages/DeleteBook"
-import EditBook from "./pages/EditBook"
-import Home from "./pages/Home"
-import ShowBook from "./pages/ShowBook"
-
-
+import { Route, Routes } from "react-router-dom";
+import CreateBook from "./pages/CreateBook";
+import DeleteBook from "./pages/DeleteBook";
+import EditBook from "./pages/EditBook";
+import Home from "./pages/Home";
+import ShowBook from "./pages/ShowBook";
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home/>}/>
-      <Route path="/books/create" element={<CreateBook/>}/>
-      <Route path="/books/delete/:id" element={<DeleteBook/>}/>
-      <Route path="/books/edit/:id" element={<EditBook/>}/>
-      <Route path="/books/details/:id" element={<ShowBook/>}/>
+      <Route path="/" element={<Home />} />
+      <Route path="/books/create" element={<CreateBook />} />
+      <Route path="/books/edit/:id" element={<EditBook />} />
+      <Route path="/books/details/:id" element={<ShowBook />} />
+      <Route path="/books/delete/:id" element={<DeleteBook />} />
     </Routes>
-  )
-}
+  );
+};
 
-export default App
+export default App;
